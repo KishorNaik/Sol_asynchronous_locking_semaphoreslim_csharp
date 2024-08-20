f8fd8cbf542bd84dd8a85400873a309f0cd2b2da
Kishor Naik <kishor.naik011.net@gmail.com>
2024-08-20 12:23:38 +0530

    @KishorNaik : Done

---
+++ Sol_Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Hello, World!");
+
+var bankObj = new Bank();
+
+await bankObj.Deposit(100);
+await bankObj.Withdrawal(50);
+
+await bankObj.Deposit(200);
+await bankObj.Withdrawal(100);
+
+public class Bank{
+    private static readonly SemaphoreSlim _lock = new SemaphoreSlim(initialCount:1,maxCount:1);
+
+    private decimal _balance = 0;
+
+    public async Task Deposit(decimal amount){
+        await _lock.WaitAsync();
+        try{
+            _balance += amount;
+            Console.WriteLine($"Balance: {_balance}");
+        }
+        finally{
+            _lock.Release();
+            Console.WriteLine("Deposit Lock Released");
+        }
+    }
+
+    public async Task Withdrawal (decimal amount){
+        await _lock.WaitAsync();
+        try{
+            _balance -= amount;
+            Console.WriteLine($"Balance: {_balance}");
+        }
+        finally{
+            _lock.Release();
+            Console.WriteLine("Withdrawal Lock Released");
+        }
+    }
+}
